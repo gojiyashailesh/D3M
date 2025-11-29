cc30babf47271cb61d84619c72ac339025ca176e
gojiyashailesh <gojiyaraju25@gmail.com>
2025-11-29 10:31:56 +0530

    :sparkles: (feat): ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 import authRoutes from './routes/auth.routes.js';
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+    app.get(/.*/, (_, res) => {
+        return res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
+    });
+}
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 npm create vite@latest . first command 
+npm run build to make the dist app folder inside the react-app folder
+
